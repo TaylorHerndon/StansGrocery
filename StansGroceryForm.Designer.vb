e25681ccbec57cbb21f4c3936a471d89c87d4a60
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-03-29 21:56:10 -0600

    Add project files.

---
+++ StansGroceryForm.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StansGroceryForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutTopMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.FilterComboBox = New System.Windows.Forms.ComboBox()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.LookUpLabel = New System.Windows.Forms.Label()
+        Me.SelectLabel = New System.Windows.Forms.Label()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.LeftGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.ContextMenuStripA = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SearchToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TopMenuStrip.SuspendLayout()
+        Me.LeftGroupBox.SuspendLayout()
+        Me.ContextMenuStripA.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TopMenuStrip
+        '
+        Me.TopMenuStrip.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileTopMenuItem, Me.HelpTopMenuItem})
+        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.TopMenuStrip.Name = "TopMenuStrip"
+        Me.TopMenuStrip.Size = New System.Drawing.Size(938, 33)
+        Me.TopMenuStrip.TabIndex = 0
+        Me.TopMenuStrip.Text = "MenuStrip1"
+        '
+        'FileTopMenuItem
+        '
+        Me.FileTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SearchToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileTopMenuItem.Name = "FileTopMenuItem"
+        Me.FileTopMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.FileTopMenuItem.Text = "&File"
+        '
+        'SearchToolStripMenuItem
+        '
+        Me.SearchToolStripMenuItem.Name = "SearchToolStripMenuItem"
+        Me.SearchToolStripMenuItem.Size = New System.Drawing.Size(166, 34)
+        Me.SearchToolStripMenuItem.Text = "&Search"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(166, 34)
+        Me.ExitToolStripMenuItem.Text = "&Exit"
+        '
+        'HelpTopMenuItem
+        '
+        Me.HelpTopMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutTopMenuItem})
+        Me.HelpTopMenuItem.Name = "HelpTopMenuItem"
+        Me.HelpTopMenuItem.Size = New System.Drawing.Size(65, 29)
+        Me.HelpTopMenuItem.Text = "&Help"
+        '
+        'AboutTopMenuItem
+        '
+        Me.AboutTopMenuItem.Name = "AboutTopMenuItem"
+        Me.AboutTopMenuItem.Size = New System.Drawing.Size(164, 34)
+        Me.AboutTopMenuItem.Text = "&About"
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.SearchTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources
+        Me.SearchTextBox.Location = New System.Drawing.Point(135, 63)
+        Me.SearchTextBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(614, 26)
+        Me.SearchTextBox.TabIndex = 1
+        '
+        'FilterComboBox
+        '
+        Me.FilterComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.FilterComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.FilterComboBox.FormattingEnabled = True
+        Me.FilterComboBox.Location = New System.Drawing.Point(135, 103)
+        Me.FilterComboBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.FilterComboBox.Name = "FilterComboBox"
+        Me.FilterComboBox.Size = New System.Drawing.Size(325, 28)
+        Me.FilterComboBox.TabIndex = 2
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(471, 103)
+        Me.DisplayListBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(434, 384)
+        Me.DisplayListBox.TabIndex = 3
+        '
+        'LookUpLabel
+        '
+        Me.LookUpLabel.AutoSize = True
+        Me.LookUpLabel.Location = New System.Drawing.Point(20, 63)
+        Me.LookUpLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LookUpLabel.Name = "LookUpLabel"
+        Me.LookUpLabel.Size = New System.Drawing.Size(105, 20)
+        Me.LookUpLabel.TabIndex = 4
+        Me.LookUpLabel.Text = "Look Up Item"
+        '
+        'SelectLabel
+        '
+        Me.SelectLabel.AutoSize = True
+        Me.SelectLabel.Location = New System.Drawing.Point(45, 106)
+        Me.SelectLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.SelectLabel.Name = "SelectLabel"
+        Me.SelectLabel.Size = New System.Drawing.Size(80, 20)
+        Me.SelectLabel.TabIndex = 5
+        Me.SelectLabel.Text = "Item Filter"
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(760, 63)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(147, 31)
+        Me.SearchButton.TabIndex = 6
+        Me.SearchButton.Text = "&Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'LeftGroupBox
+        '
+        Me.LeftGroupBox.Controls.Add(Me.DisplayLabel)
+        Me.LeftGroupBox.Location = New System.Drawing.Point(135, 144)
+        Me.LeftGroupBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.LeftGroupBox.Name = "LeftGroupBox"
+        Me.LeftGroupBox.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.LeftGroupBox.Size = New System.Drawing.Size(327, 345)
+        Me.LeftGroupBox.TabIndex = 7
+        Me.LeftGroupBox.TabStop = False
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.Location = New System.Drawing.Point(4, 25)
+        Me.DisplayLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(314, 315)
+        Me.DisplayLabel.TabIndex = 0
+        '
+        'ContextMenuStripA
+        '
+        Me.ContextMenuStripA.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStripA.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SearchToolStripMenuItem1, Me.ExitToolStripMenuItem1})
+        Me.ContextMenuStripA.Name = "ContextMenuStrip"
+        Me.ContextMenuStripA.Size = New System.Drawing.Size(137, 68)
+        '
+        'SearchToolStripMenuItem1
+        '
+        Me.SearchToolStripMenuItem1.Name = "SearchToolStripMenuItem1"
+        Me.SearchToolStripMenuItem1.Size = New System.Drawing.Size(136, 32)
+        Me.SearchToolStripMenuItem1.Text = "Search"
+        '
+        'ExitToolStripMenuItem1
+        '
+        Me.ExitToolStripMenuItem1.Name = "ExitToolStripMenuItem1"
+        Me.ExitToolStripMenuItem1.Size = New System.Drawing.Size(136, 32)
+        Me.ExitToolStripMenuItem1.Text = "Exit"
+        '
+        'StansGroceryForm
+        '
+        Me.AcceptButton = Me.SearchButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(938, 514)
+        Me.ContextMenuStrip = Me.ContextMenuStripA
+        Me.Controls.Add(Me.LeftGroupBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SelectLabel)
+        Me.Controls.Add(Me.LookUpLabel)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.FilterComboBox)
+        Me.Controls.Add(Me.SearchTextBox)
+        Me.Controls.Add(Me.TopMenuStrip)
+        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "StansGroceryForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Welcome to Stan's Grocery"
+        Me.TopMenuStrip.ResumeLayout(False)
+        Me.TopMenuStrip.PerformLayout()
+        Me.LeftGroupBox.ResumeLayout(False)
+        Me.ContextMenuStripA.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TopMenuStrip As MenuStrip
+    Friend WithEvents FileTopMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpTopMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutTopMenuItem As ToolStripMenuItem
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents FilterComboBox As ComboBox
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents LookUpLabel As Label
+    Friend WithEvents SelectLabel As Label
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents LeftGroupBox As GroupBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents SearchToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStripA As ContextMenuStrip
+    Friend WithEvents SearchToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ StansGroceryForm.vb
@@ -0,0 +1,182 @@
+﻿Option Strict On
+Option Explicit On
+
+'Taylor Herndon
+'RCET0265
+'Spring 2021
+'Stans Grocery
+'
+
+Public Class StansGroceryForm
+
+    Dim temp(999) As String
+    Dim food(999, 2) As String
+    Sub StartupProcesses() Handles Me.Load
+
+        temp = Split(StansGrocery.My.Resources.Grocery, vbLf, -1)
+
+        'Assign food names, aisle number, and category
+        For i = 0 To temp.Length - 1
+
+            'Index the food name
+            food(i, 0) = temp(i).Substring(6, temp(i).IndexOf(",") - 7)
+
+            'Index the food aisle
+            food(i, 1) = temp(i).Substring(temp(i).IndexOf(",") + 7, temp(i).LastIndexOf(",") - temp(i).IndexOf(",") - 8)
+
+            'Index the food category
+            food(i, 2) = temp(i).Substring(temp(i).LastIndexOf(",") + 7, temp(i).Length - temp(i).LastIndexOf(",") - 8)
+
+        Next
+
+        'Add each unique category to the filter combo box
+        For i = 0 To temp.Length - 1
+
+            If Not FilterComboBox.Items.Contains(food(i, 2)) And food(i, 2) <> "" Then
+
+                FilterComboBox.Items.Add(food(i, 2))
+
+            End If
+
+        Next
+
+        FilterComboBox.Items.Add("All Items") 'Add "All Items" to the filter combo box
+        FilterComboBox.Sorted = True 'Sort the filter combo box by alphabetical order
+        FilterComboBox.SelectedItem = "All Items" 'Set the on startup filter to all items
+
+        DisplayListOnCategory("All Items") 'Update listed items to the all items category
+
+    End Sub
+
+    Sub SearchSubmit() Handles SearchButton.Click, SearchToolStripMenuItem.Click, SearchToolStripMenuItem1.Click
+
+        Dim selectedItemNumber As Integer = 0
+
+        FilterComboBox.SelectedItem = "All Items" 'When a search is submitted reset the filter to all items
+
+        'If zzz is detected end the program
+        If SearchTextBox.Text = "zzz" Then
+
+            End
+
+        End If
+
+        'Search the food array for the searched item
+        For i = 0 To temp.Length - 1
+
+            If food(i, 0) = SearchTextBox.Text And food(i, 0) <> "" Then
+
+                selectedItemNumber = i 'If a match is found set the selected item number to the current index
+                Exit For 'Exit the for loop
+
+            End If
+
+            If i = temp.Length - 1 Then
+
+                selectedItemNumber = -1 'If no match was found then set the selected item to -1
+
+            End If
+
+        Next
+
+        If selectedItemNumber = -1 Then
+
+            DisplayLabel.Text = "Item could not be found." 'If item could not be found inform the user
+
+        Else
+
+            'If a match was found then fill out the item name, aisle name, and category to the display label
+            DisplayLabel.Text = food(selectedItemNumber, 0) &
+                                " can be found in aisle " & food(selectedItemNumber, 1) &
+                                " with the " & food(selectedItemNumber, 2)
+
+        End If
+
+    End Sub
+
+    Sub DisplayListOnCategory(category As String)
+
+        DisplayListBox.Items.Clear() 'Clear the item list
+
+        'All items was selected then fill out the item list with all items
+        If category = "All Items" Then
+
+            For i = 0 To temp.Length - 1
+
+                If food(i, 0) <> "" Then
+
+                    DisplayListBox.Items.Add(food(i, 0))
+
+                End If
+
+            Next
+
+            DisplayListBox.Sorted = True 'Sort the item list by alphabetical order
+            Exit Sub 'Exit the sub
+
+        End If
+
+
+
+        For i = 0 To temp.Length
+
+            'Add all of the items with the selected category to the item list box
+            If food(i, 0) <> "" And food(i, 2) = category Then
+
+                DisplayListBox.Items.Add(food(i, 0))
+
+            End If
+
+            DisplayListBox.Sorted = True 'Sort the item list by alphabetical order
+
+        Next
+
+    End Sub
+
+    Sub UpdateDisplayBoxFromListBox() Handles DisplayListBox.Click
+
+        Dim selectedItemNumber As Integer = 0
+
+        'Search the food array for the selected item
+        For i = 0 To temp.Length - 1
+
+            If food(i, 0) Is DisplayListBox.SelectedItem Then
+
+                selectedItemNumber = i 'Once the selected item has been found set the item number to the current index
+                Exit For 'Exit the for loop
+
+            End If
+
+        Next
+
+        'Fill out the item name, aisle name, and category to the display label
+        DisplayLabel.Text = food(selectedItemNumber, 0) &
+                            " can be found in aisle " & food(selectedItemNumber, 1) &
+                            " with the " & food(selectedItemNumber, 2)
+
+    End Sub
+
+    Sub UpdateDisplayListBox() Handles FilterComboBox.SelectedIndexChanged
+
+        DisplayListOnCategory(FilterComboBox.SelectedItem.ToString) 'Update the item list box to only display the selected category
+
+    End Sub
+
+    Sub Help() Handles AboutTopMenuItem.Click
+
+        'Display a message box with information about the search engine
+        MsgBox("Welcome to Stan's Grocery search engine!" & vbNewLine & vbNewLine &
+               "To search for a specific item type your item name into the text box labeled 'Look Up Item'." & vbNewLine & vbNewLine &
+               "You can also select an item by clicking the item in the item list on the right." & vbNewLine & vbNewLine &
+               "To trim down your item selection choose a category with the 'Item Filter' box." & vbNewLine & vbNewLine &
+               "Your item, aisle, and similar items will be displayed under the 'Item Filter' box.")
+
+    End Sub
+
+    Sub ExitProgram() Handles ExitToolStripMenuItem.Click, ExitToolStripMenuItem1.Click
+
+        End
+
+    End Sub
+
+End Class
